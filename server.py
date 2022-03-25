a128e375d21356b742c4bfbc33c3b3b44c97a758
Isbo <94263966+Isbo2000@users.noreply.github.com>
2022-03-24 20:18:15 -0400

    initial commit

---
+++ server.py
@@ -0,0 +1,87 @@
+import os
+import sys
+import praw
+import json
+import getpass
+from time import sleep
+import firebase_admin
+from firebase_admin import credentials
+print("Starting script...\n")
+try:
+    firebase_admin.initialize_app(
+        credentials.Certificate("firebase-login.json")
+    )
+except:
+    print("Please add or fix 'firebase-login.json'")
+    sys.exit()
+
+def redlog(login):
+    return praw.Reddit(
+        client_id = login["id"], 
+        client_secret = login["secret"], 
+        username = login["username"], 
+        password = login["password"], 
+        user_agent = "Isbo2000 Coddit 5.0")
+def asklogin():
+    i = getpass.getpass('Id: ')
+    s = getpass.getpass('Secret: ')
+    u = input('Username: ')
+    p = getpass.getpass('Password: ')
+    login = {"id":i,"secret":s,"username":u,"password":p}
+    with open('login.json', 'w') as lgn:json.dump(login, lgn)
+    try:redlog(login).user.me()
+    except:
+        os.remove('login.json')
+        sys.exit()
+    return redlog(login)
+def checklogin():
+    if os.path.exists('login.json'):
+        with open('login.json') as lgn:login=json.load(lgn)
+        try:redlog(login).user.me()
+        except:return asklogin()
+    else:return asklogin()
+    return redlog(login)
+reddit = checklogin()
+
+sub_stream = reddit.subreddit("teenagersbutpog").stream.submissions(pause_after = 0, skip_existing = True)
+com_stream = reddit.subreddit("teenagersbutpog").stream.comments(pause_after = 0, skip_existing = True)
+banned = ["Isbot2000","DimittrikovBot","AutoModerator"]
+
+def counter(stream, con_type):
+    for thing in stream:
+        if (not(thing)):return
+        author = str(thing.author)
+        if (author in banned):return
+        with open('static/data.json') as data_file:
+            data=json.load(data_file)
+        with open('static/all-time.json') as all_data_file:
+            all_data=json.load(all_data_file)
+        logs = [data,all_data]
+        for log in logs:
+            if (author in log):
+                log[author][con_type] +=1
+            else:
+                log[author] = [0, 0]
+                log[author][con_type] +=1
+            if(log==data):
+                with open('static/data.json','w') as data_file:
+                    json.dump(log,data_file,indent=3)
+            elif(log==all_data):
+                with open('static/all-time.json','w') as all_data_file:
+                    json.dump(log,all_data_file,indent=3)
+        if(con_type==0):ty="Submission"
+        elif(con_type==1):ty="Comment"
+        print(f'{ty} added for {author}')
+        sleep(1)
+
+print("Ready\n")
+while True:
+    try:
+        counter(sub_stream,0)
+        sleep(5)
+        counter(com_stream,1)
+        sleep(5)
+    except BaseException as error:
+        print(str(error))
+        sleep(30)
+
