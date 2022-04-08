b4d28e4137660fcdc4cfe0855a03cf7255711b21
Isbo <94263966+Isbo2000@users.noreply.github.com>
2022-04-08 16:14:28 -0400

    redid the server script, hopefully It didnt break

--- server.py
+++ server.py
@@ -36,21 +36,21 @@ def checklogin():
 subreddit = checklogin().subreddit("teenagersbutpog")
 banned = ["Isbot2000", "DimittrikovBot", "AutoModerator"]
 datdbs = [db.reference("data"), db.reference("all-time")]
-sub_stream = subreddit.stream.submissions(pause_after = 10, skip_existing = True)
-com_stream = subreddit.stream.comments(pause_after = 10, skip_existing = True)
-streams = [
-    {"content":sub_stream, "name":"Submission", "num":0},
-    {"content":com_stream, "name":"Comment", "num":1}
-]
 print("Ready\n")
 
 while True:
     try:
-        sub_stream = subreddit.stream.submissions(pause_after = 10, skip_existing = True)
-        com_stream = subreddit.stream.comments(pause_after = 10, skip_existing = True)
         streams = [
-            {"content":sub_stream, "name":"Submission", "num":0},
-            {"content":com_stream, "name":"Comment", "num":1}
+            {
+                "content":subreddit.stream.submissions(pause_after = 0, skip_existing = True),
+                "name":"Submission",
+                "num":0
+            },
+            {
+                "content":subreddit.stream.comments(pause_after = 0, skip_existing = True),
+                "name":"Comment",
+                "num":1
+            }
         ]
         for stream in streams:
             for con in stream["content"]:
@@ -66,9 +66,7 @@ while True:
                                 data[author][stream["num"]] += 1
                             datdb.set(data)
                         print(stream["name"]+" added for "+author)
-                        time.sleep(5)
                     else:print(author+" is banned, nothing added")
-                else:time.sleep(10)
     except BaseException as error:
         print(str(error))
         time.sleep(30)
