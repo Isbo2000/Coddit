97f0b887e0b2e4315623211aa9f22dfcd8428c6e
Isbo <94263966+Isbo2000@users.noreply.github.com>
2022-04-08 16:32:15 -0400

    yeah, i broke it

--- server.py
+++ server.py
@@ -38,33 +38,29 @@ banned = ["Isbot2000", "DimittrikovBot", "AutoModerator"]
 datdbs = [db.reference("data"), db.reference("all-time")]
 print("Ready\n")
 
+def counter(stream, name, num):
+    for con in stream:
+        if con is not None:
+            author = str(con.author)
+            if (author not in banned):
+                for datdb in datdbs:
+                    data = datdb.get()
+                    if (author in data):
+                        data[author][num] += 1
+                    else:
+                        data[author] = [0, 0]
+                        data[author][num] += 1
+                    datdb.set(data)
+                print(name+" added for "+author)
+            else:print(author+" is banned, nothing added");continue
+        else:time.sleep(1);continue
 while True:
     try:
-        streams = [
-            {
-                "content":subreddit.stream.submissions(skip_existing = True),
-                "name":"Submission",
-                "num":0
-            },
-            {
-                "content":subreddit.stream.comments(skip_existing = True),
-                "name":"Comment",
-                "num":1
-            }
-        ]
-        for stream in streams:
-            for con in stream["content"]:
-                author = str(con.author)
-                if (author not in banned):
-                    for datdb in datdbs:
-                        data = datdb.get()
-                        if (author in data):
-                            data[author][stream["num"]] += 1
-                        else:
-                            data[author] = [0, 0]
-                            data[author][stream["num"]] += 1
-                        datdb.set(data)
-                    print(stream["name"]+" added for "+author)
-                else:print(author+" is banned, nothing added")
+        sub_stream = subreddit.stream.submissions(pause_after = 0, skip_existing = True)
+        com_stream = subreddit.stream.comments(pause_after = 0, skip_existing = True)
+        counter(sub_stream,"Submission",0)
+        time.sleep(1)
+        counter(com_stream,"Comment",1)
+        time.sleep(1)
     except BaseException as error:
         print(str(error))
