4cb0b4c535594f8b3cf4b35baae86aa794561093
Isbo2000 <94263966+Isbo2000@users.noreply.github.com>
2023-04-02 18:50:32 -0400

    added data file thingy

---
+++ Website/data.js
@@ -0,0 +1,117 @@
+var years = [
+    {"name": "2021",
+        "tooltip": "Due to coddit not running the whole year data from 2021 is incomplete"
+    },
+    {"name": "2022",
+        "tooltip": "Due to coddit not running the whole year data from 2022 is incomplete"
+    }
+]
+
+var months = [
+    {"name": "October 2021"},
+    {"name": "November 2021",
+        "tooltip": "Due to technical problems data for November 2021 is inaccurate"
+    },
+    {"name": "December 2021",
+        "tooltip": "Due to technical problems data for December 2021 is inaccurate"
+    },
+
+    {"name": "January 2022"},
+    {"name": "February 2022"},
+    {"name": "March 2022"},
+    {"name": "April 2022"},
+    {"name": "May 2022"},
+    {"name": "June 2022"},
+    {"name": "July 2022",
+        "tooltip": "Due to Isbo being stupid, this includes data from some of August 2022 as well"
+    },
+    {"name": "October 2022"},
+    {"name": "November 2022"},
+    {"name": "December 2022"},
+
+    {"name": "January 2023"},
+    {"name": "February 2023"},
+    {"name": "March 2023"}
+]
+
+var credits = [
+    {
+        "name": "Git/Jacob",
+        "url": "https://www.reddit.com/user/git-commit-die/",
+        "tooltip": "Made the original Coddit and basically started the whole thing"
+    },
+    {
+        "name": "Abe",
+        "url": "https://www.reddit.com/user/-i-hate-this-place-/",
+        "tooltip": "Remade Coddit based off of Git/Jacob's original Coddit"
+    },
+    {
+        "name": "Dimittrikov",
+        "url": "https://www.reddit.com/user/Dimittrikov1995/",
+        "tooltip": "Took over from Abe and developed most of the current Coddit"
+    },
+    {
+        "name": "Daniel_R013",
+        "url": "https://www.reddit.com/user/Daniel_R013/",
+        "tooltip": "Developed most of the current look"
+    },
+    {
+        "name": "Isbo2000",
+        "url": "https://www.reddit.com/user/Isbo2000/",
+        "tooltip": "Currently maintaining and hosting Coddit"
+    }
+]
+
+var splash = [
+    "A break from reddit every so often is always helpful!",
+    "All things are difficult before they are easy",
+    "Be you",
+    "Be yourself more",
+    "Call your local crisis line when things get especially sticky",
+    "Chill oooouuuuutttt B)",
+    "Don't light yourself on fire to keep others warm",
+    "Good work!",
+    "Hai B)",
+    "HEY! YOU, YES YOU, YOU. YOU'RE AMAZING!",
+    "Holding on to anger is like drinking poison and hoping the other person dies",
+    "I like your style",
+    "Impressive",
+    "In through the nose and out through the mouth",
+    "It's important to stay independent",
+    "It's only embarrassing if you're embarrassed",
+    "Just keep swimming Just keep swimming Just keep swimming...",
+    "Keep it up",
+    "Keep on swimming",
+    "People who are goodlooking but have terrible personalities are basically real life clickbaits",
+    "Pleasure to meet you",
+    "Remember to breathe",
+    "r/teenagersbutpog, since April 10, 2021",
+    "Show the world who you are, because who you are is an amazing person",
+    "Square up!",
+    "Stay true to you!",
+    "Take breaks!",
+    "Talk to a trusted family member or friend whenever you get into a pickle",
+    "There's a tbp discord, go check it out!",
+    "The secret of getting ahead is getting started",
+    "Those who matter don't mind and those who mind don't matter",
+    "Treat yourself",
+    "Treat yourself you deserve it",
+    "Welcome!",
+    "We love having you around",
+    "We love you for you",
+    "When life shuts a door, open it again, it's a door, that's how they work",
+    "When something is important enough, you do it even if the odds are not in your favor",
+    "Woah, impressive",
+    "Woah, quite the reddit usage there!",
+    "Work it!",
+    "You are an incredible person",
+    "You look wonderful! You should show everyone sometime",
+    "You make people's days",
+    "You matter",
+    "You matter to us",
+    "You're perfect just the way you are",
+    "You're probably smart enough to do a crossword puzzle in pen",
+    "You're worthy",
+    "Your friends probably love you more than you realize, damn",
+    "You will meet people that see a lot more in you than you do in yourself"
+]
